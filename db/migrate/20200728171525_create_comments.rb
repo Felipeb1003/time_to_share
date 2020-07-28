46b199f304dfefcfd84e9a5ef8ad4eb38398bc51
Felipe <felipebeltran.nyc@gmail.com>
2020-07-28 17:49:27 -0400

    New Schema

--- db/migrate/20200728171525_create_comments.rb
+++ db/migrate/20200728171525_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.belongs_to :user
+      t.belongs_to :post
+      t.integer :rating
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_174202) do
+ActiveRecord::Schema.define(version: 2020_07_28_171525) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -19,15 +19,14 @@ ActiveRecord::Schema.define(version: 2020_07_28_174202) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
     t.integer "post_id"
+    t.integer "rating"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "comment_content"
-  end
-
-  create_table "comments_users", id: false, force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "comment_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
