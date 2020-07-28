0589060def3751bb8baf8b381d15aeb89007054e
Felipe <felipebeltran.nyc@gmail.com>
2020-07-28 13:05:28 -0400

    generate Post

---
+++ db/migrate/20200728170353_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.date :date
+      t.string :location
+      t.text :review
+      t.integer :user_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
