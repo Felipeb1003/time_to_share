45845fe00b0cb86d44e00aabe0b5f0a9d9502da8
Felipe <felipebeltran.nyc@gmail.com>
2020-07-28 17:47:36 -0400

    Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    belongs_to :category
+
+    has_many :comments
+    has_many :commenters, through: :comments, source: :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+
+    has_many :comments
+    has_many :comented_posts, through: :comments, source: :post
 end
