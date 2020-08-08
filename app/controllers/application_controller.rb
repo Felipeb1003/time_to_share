e63ef3bb7cd597a2c900fb1aed510d58b6b49883
Felipe <felipebeltran.nyc@gmail.com>
2020-08-08 17:27:56 -0400

    Logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    include ApplicationHelper
+    def checked_log_in
+        if !logged_in?
+            flash[:message] = "Not Logged in!"
+            redirect_to '/log_in'
+        end
+    end
+
+    
 end
