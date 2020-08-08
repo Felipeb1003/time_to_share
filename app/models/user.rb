cf5fdf6a14565fc3661ced1ca830dea11a352807
Felipe <felipebeltran.nyc@gmail.com>
2020-08-08 17:29:04 -0400

    Add Validations and Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
 
     has_many :comments
     has_many :comented_posts, through: :comments, source: :post
+
+    has_secure_password
+    validates :email, :username, presence: :true, uniqueness: true
+    
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |user|
+            user.username = auth['info']['name']
+            user.email = auth['info']['email']
+            user.password = SecureRandom.hex(20)
+        end
+    end
+
+
 end
