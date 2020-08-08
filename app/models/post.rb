999110f8df2fdd6ce9d35e47bc9cc87899d654df
Felipe <felipebeltran.nyc@gmail.com>
2020-08-08 17:31:42 -0400

    Validations and query methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ApplicationRecord
 
     has_many :comments
     has_many :commenters, through: :comments, source: :user
+    
+    validates :title, :date, :location, presence: :true
+    
+   
+    def self.ordered_list
+        self.order(created_at: :asc)
+    end
 
+    def self.todays_post
+        where("created_at >= ?", Time.zone.now.beginning_of_day)
+    end
+
+    
 end
